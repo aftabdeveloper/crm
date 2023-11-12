71b019abee594a49c02972f6cc1773faefd20fd3
Aftab Alam <alam69745@gmail.com>
2023-11-12 20:50:57 +0530

    login signup router redirection middleware

--- middleware.js
+++ middleware.js
@@ -5,27 +5,54 @@ export const middleware = async (req)=>{
     const authToken= req.cookies.has("authToken")
 
     if(!authToken) return res.redirect(new URL('/login', req.url))
+    const {value} = req.cookies.get("authToken")
 
-    try
-    {
-        const {value} = req.cookies.get("authToken")
-        const http = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/jwt/${value}`)
-        if(http.status !== 200) return res.redirect(new URL('/login', req.url))
-        const session = await http.json()
-        const header = res.next()
-        header.cookies.set({
-            name: "session",
-            value: JSON.stringify(session),
-            path: "/"
-        })
-        return header
+    if (req.nextUrl.pathname.startsWith('/admin')) {
+        try
+        {
+            const http = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/jwt/${value}`)
+            if(http.status !== 200) return res.redirect(new URL('/login', req.url))
+            const session = await http.json()
+            const header = res.next()
+            header.cookies.set({
+                name: "session",
+                value: JSON.stringify(session),
+                path: "/"
+            })
+            return header
+        }
+        catch(err)
+        {
+            return res.redirect(new URL('/login', req.url))
+        }
     }
-    catch(err)
-    {
-        return res.redirect(new URL('/login', req.url))
+    
+    if (req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/signup')) {
+        try
+        {
+            const http = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/jwt/${value}`)
+            if(http.status !== 200) return res.next()
+            const session = await http.json()
+            const header = res.next()
+            header.cookies.set({
+                name: "session",
+                value: JSON.stringify(session),
+                path: "/"
+            })
+            return (
+                header,
+                res.redirect(new URL('/admin',req.url))
+            )
+
+        }
+        catch(err)
+        {
+            return res.next()
+        }  
     }
+
 }
 
 export const config = {
-    matcher: '/admin',
+    matcher: ['/admin','/login','/signup']
 }
